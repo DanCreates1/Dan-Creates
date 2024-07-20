a375fe9eefedbcfc34a6e30529178d0669069f07
Pouyan <154380480+Mrengin1@users.noreply.github.com>
2024-07-19 22:16:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -656,3 +656,30 @@ animateTyping("I am Pouyan, a full stack developer", 0, wordAnimationDiv);
 
 setInterval(updateAgeAndTimer, 100);
 updateAgeAndTimer();
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+  
+    burger.addEventListener('click', () => {
+      // Toggle Nav
+      nav.classList.toggle('nav-active');
+  
+      // Animate Links
+      navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = '';
+        } else {
+          link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+        }
+      });
+  
+      // Burger Animation
+      burger.classList.toggle('toggle');
+    });
+  });
+  
